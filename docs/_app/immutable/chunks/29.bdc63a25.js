cd579727579ddbd83f8ed596529458b3c23e89aa
beerlincur <zhozhpost@gmail.com>
2023-05-06 15:41:22 +0300

    FIXED double "need to" on "If statements" page [builded]

--- docs/_app/immutable/chunks/29.bdc63a25.js
+++ docs/_app/immutable/chunks/29.bdc63a25.js
@@ -1,1 +1,5 @@
+<<<<<<<< HEAD:docs/_app/immutable/chunks/29.bdc63a25.js
 import{default as t}from"../entry/(examples)-04-if-statements-page.svelte.c7e8f278.js";export{t as component};
+========
+import{default as t}from"../entry/(examples)-04-if-statements-page.svelte.dd50a5ed.js";export{t as component};
+>>>>>>>> 58a3455 (FIXED double "need to" on "If statements" page [builded]):docs/_app/immutable/chunks/29.2c63852b.js
