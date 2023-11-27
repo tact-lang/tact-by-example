c75ac5019353739c6be83ca9cebd3990e17f0902
Anton Trunov <anton@ton.org>
2023-11-27 12:26:26 +0400

    Fix comments for shl/shr

---
+++ docs/_app/immutable/chunks/1.82965511.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.1692fb6c.js";export{t as component};
--- docs/_app/immutable/chunks/4.5049ab46.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.7a7d4d05.js";export{t as component};
